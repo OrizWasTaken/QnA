eb06081df23a9fce076e861a9817a70c7ea2d7b3
Joseph Orisakwe <oriz4work@gmail.com>
2025-08-15 18:35:48 +0100

    test(qnas): standardize test docstrings for clarity and consistency

--- qnas/tests.py
+++ qnas/tests.py
@@ -7,53 +7,57 @@ from django.contrib.auth import get_user_model
 
 from .models import Tag, Question, QuestionVote, Answer, AnswerVote, View
 
+
 def _assert_successful_request(obj, view, query_params=None, **kwargs):
-    """send a request to a url and assert that the response is 200 (successful)"""
+    """Send a GET request to the given view and assert a 200 OK response."""
     response = obj.client.get(reverse(view, kwargs=kwargs), query_params=query_params)
     obj.assertEqual(response.status_code, 200)
     return response
 
+
 class QuestionModelTests(TestCase):
-    def setUp(self):
-        self.now = timezone.now()
+    @classmethod
+    def setUpTestData(cls):
+        cls.now = timezone.now()
 
     def test_is_edited_with_unedited_question(self):
-        """is_edited() should return False if the question has not been edited."""
-        pub_date = self.now
-        mod_date = pub_date
-        unedited_question = Question(pub_date=pub_date, mod_date=mod_date)
-        self.assertIs(unedited_question.is_edited, False)
-
-    def test_is_edited_ignores_microseconds_in_comparison(self):
         """
-           is_edited() should return False when the only difference
-           between pub_date and mod_date is less than one second (microseconds difference).
+        is_edited should return False if pub_date and mod_date are identical
+        or the only difference between them is less than one second
+        (microsecond differences ignored).
 
-           This verifies that microsecond-level variations, which can occur when both
-           timestamps are set during object creation, are ignored in the edited check.
-       """
-        pub_date = self.now - datetime.timedelta(microseconds=999_999)
-        mod_date = self.now
-        unedited_question = Question(pub_date=pub_date, mod_date=mod_date)
-        self.assertIs(unedited_question.is_edited, False)
+        This verifies that microsecond-level variations, which can occur when both
+        timestamps are set during object creation, are ignored in the edited check.
+        """
+        with self.subTest("pub_date and mod_date are identical"):
+            mod_date = pub_date = self.now
+            unedited_question = Question(pub_date=pub_date, mod_date=mod_date)
+            self.assertIs(unedited_question.is_edited, False)
+
+        with self.subTest("pub_date and mod_date differ by less than a second"):
+            pub_date = self.now - datetime.timedelta(microseconds=999_999)
+            mod_date = self.now
+            unedited_question = Question(pub_date=pub_date, mod_date=mod_date)
+            self.assertIs(unedited_question.is_edited, False)
 
     def test_is_edited_with_edited_questions(self):
-        """is_edited() should return True if the question has been edited."""
+        """is_edited should return True if mod_date is later than pub_date by ≥ 1 second."""
         pub_date = self.now - datetime.timedelta(minutes=10)
         mod_date = self.now
         edited_question = Question(pub_date=pub_date, mod_date=mod_date)
         self.assertIs(edited_question.is_edited, True)
 
-class AnswerStrMethodTest(TestCase):
+
+class AnswerModelTests(TestCase):
     def test_str_with_short_text(self):
-        """__str__ should return the stripped text if ≤ 200 chars."""
+        """__str__ should return the cleaned text if ≤ 200 characters."""
         short_text = "  This is a short answer.  \n  With a second line.  "
-        answer = Answer(text=short_text,)
+        answer = Answer(text=short_text)
         expected = "This is a short answer. With a second line."
         self.assertEqual(str(answer), expected)
 
     def test_str_with_long_text(self):
-        """__str__ should truncate to 200 chars with ellipsis if longer."""
+        """__str__ should return the first 200 characters followed by '...' if text is longer."""
         long_text = "line1\n" + "a" * 250
         answer = Answer(text=long_text)
         stripped_joined = "line1 " + "a" * 250
@@ -61,26 +65,33 @@ class AnswerStrMethodTest(TestCase):
         self.assertEqual(str(answer), expected)
 
     def test_str_removes_blank_lines(self):
-        """__str__ should remove blank lines entirely."""
+        """__str__ should remove all blank lines when joining text."""
         answer = Answer(text="First line\n\n \nSecond line")
         expected = "First line Second line"
         self.assertEqual(str(answer), expected)
 
+
 class VoteCountTests(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.author, = get_user_model().objects.bulk_create([get_user_model()(username=f"author")])
-        cls.question = Question.objects.create(author=cls.author, title="test_question", body="test_body")
-        cls.answer = Answer.objects.create(author=cls.author, question=cls.question, text="test_answer")
+        cls.author, = get_user_model().objects.bulk_create(
+            [get_user_model()(username="author")]
+        )
+        cls.question = Question.objects.create(
+            author=cls.author, title="test_question", body="test_body"
+        )
+        cls.answer = Answer.objects.create(
+            author=cls.author, question=cls.question, text="test_answer"
+        )
 
     def _assert_vote_count_without_votes_for(self, content):
-        """assert vote_count should return 0 when there are no votes for `content`."""
+        """vote_count should return 0 if there are no votes for the content."""
         self.assertEqual(content.vote_count, 0)
 
     def _assert_vote_count_with_votes_for(self, content, vote_model, fk_field_name):
         """
-        Create `vote_model` votes for a `content` with related_name `fk_field_name` and assert that vote_count
-        return the net sum (upvotes as +1, downvotes as -1) of all vote values when there are votes for `content`.
+        vote_count should return the net sum of all vote values (+1 upvote, -1 downvote)
+        when votes exist for the content.
         """
         vote_values = (1, -1, 1, 1, -1)
         users = [get_user_model()(username=f"test_user{i}") for i in range(len(vote_values))]
@@ -88,46 +99,52 @@ class VoteCountTests(TestCase):
 
         votes = [
             vote_model(user=user, value=value, **{fk_field_name: content})
-            for user, value in zip(users, vote_values)]
+            for user, value in zip(users, vote_values)
+        ]
         vote_model.objects.bulk_create(votes)
 
         self.assertEqual(content.vote_count, sum(vote_values))
 
     def test_vote_count_without_votes_for_questions(self):
+        """Questions without votes should have vote_count = 0."""
         self._assert_vote_count_without_votes_for(self.question)
 
     def test_vote_count_with_votes_for_questions(self):
+        """Questions with votes should return correct net vote count."""
         self._assert_vote_count_with_votes_for(self.question, QuestionVote, "question")
 
     def test_vote_count_without_votes_for_answers(self):
+        """Answers without votes should have vote_count = 0."""
         self._assert_vote_count_without_votes_for(self.answer)
 
     def test_vote_count_with_votes_for_answers(self):
+        """Answers with votes should return correct net vote count."""
         self._assert_vote_count_with_votes_for(self.answer, AnswerVote, "answer")
+
 
 class ViewModelTests(TestCase):
     def test_hrs_since_viewed(self):
-        """hrs_since_viewed should return the number of hours since view_time"""
+        """hrs_since_viewed should return hours since view_time, rounded to 2 decimals."""
         view = View(
-            view_time=(timezone.now() - datetime.timedelta(hours=3)) # viewed 3 hrs ago
+            view_time=(timezone.now() - datetime.timedelta(hours=3))
         )
-        self.assertAlmostEqual(view.hrs_since_viewed, 3, places=2) # 3 hrs to 2 d.p
+        self.assertAlmostEqual(view.hrs_since_viewed, 3, places=2)
+
 
 class QuestionListViewsTests(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.user, = get_user_model().objects.bulk_create((get_user_model()(username=f"author"),))
+        cls.user, = get_user_model().objects.bulk_create(
+            (get_user_model()(username="author"),)
+        )
 
     def _assert_no_question_for(self, view, **kwargs):
-        """
-        If no question exists, an appropriate text should be displayed.
-        By verifying text, asserts that the respective view functions render the right template.
-        """
+        """When no questions exist, the page should display '0 questions'."""
         response = _assert_successful_request(self, view, **kwargs)
         self.assertContains(response, "0 questions")
 
     def _assert_default_filter_for(self, view, **kwargs):
-        """questions should be ordered by `newest` if no tab is selected or the selected tab does not exist."""
+        """Questions should default to 'newest' tab if no or invalid tab is selected."""
         questions = Question.objects.bulk_create([Question(author=self.user) for _ in range(3)])
         if kwargs.get("tag"):
             kwargs["tag"].questions.add(*questions)
@@ -138,14 +155,15 @@ class QuestionListViewsTests(TestCase):
             self.assertEqual(response.context["tab"].lower(), "newest")
             self.assertQuerySetEqual(response.context["all_questions"], reversed(questions))
 
-        with self.subTest("No tab selected"): _assert_newest_is_default_tab()
-        with self.subTest("Selected undefined tab"): _assert_newest_is_default_tab(query_params={"tab": "oldest"})
+        with self.subTest("No tab selected"):
+            _assert_newest_is_default_tab()
+        with self.subTest("Invalid tab selected"):
+            _assert_newest_is_default_tab(query_params={"tab": "oldest"})
 
     def _assert_defined_tabs_for(self, view, **kwargs):
         """
-        questions should be filtered correctly when a defined, existing tag is selected.
-        tab 'unanswered' should filter questions without an answer.
-        tab 'popular' should filter questions by view count in descending order.
+        'Unanswered' tab should show only questions without answers.
+        'Popular' tab should sort questions by view count (descending).
         """
         questions = Question.objects.bulk_create([Question(author=self.user) for _ in range(3)])
         q1, q2, q3 = questions
@@ -153,58 +171,73 @@ class QuestionListViewsTests(TestCase):
             kwargs["tag"].questions.add(*questions)
             kwargs = {"tag_text": kwargs["tag"].text}
 
-        with self.subTest("Tab 'unanswered' selected"):
-            q3.answers.create(author=self.user) # create an answer for the last question.
+        with self.subTest("'Unanswered' tab"):
+            q3.answers.create(author=self.user)
             response = _assert_successful_request(self, view, query_params={"tab": "Unanswered"}, **kwargs)
             self.assertEqual(response.context["tab"], "Unanswered")
-            self.assertQuerySetEqual(response.context["all_questions"], (q2, q1)) # `all_questions` should omit q3.
+            self.assertQuerySetEqual(response.context["all_questions"], (q2, q1))
 
-        with self.subTest("Tab 'popular' selected"):
+        with self.subTest("'Popular' tab"):
             View.objects.bulk_create([View(user=self.user, question=q3) for _ in range(2)])
             View.objects.bulk_create([View(user=self.user, question=q1) for _ in range(1)])
             response = _assert_successful_request(self, view, query_params={"tab": "Popular"}, **kwargs)
             self.assertEqual(response.context["tab"], "Popular")
-            self.assertQuerySetEqual(response.context["all_questions"], (q3, q1, q2)) # questions ordered by views.
+            self.assertQuerySetEqual(response.context["all_questions"], (q3, q1, q2))
 
     def test_tagged_questions_with_nonexistent_tag(self):
+        """Nonexistent tag in tagged-questions view should return 404."""
         response = self.client.get(reverse("qnas:tagged-questions", args=["test_tag"]))
         self.assertEqual(response.status_code, 404)
 
     def test_no_question_for_tagged_questions(self):
+        """If no questions exist, '0 questions' should be displayed."""
         self._assert_no_question_for("qnas:tagged-questions", tag_text=Tag.objects.create(text="test_tag").text)
 
     def test_no_question_for_questions(self):
+        """If no questions exist, '0 questions' should be displayed."""
         self._assert_no_question_for("qnas:questions")
 
     def test_default_filter_for_questions(self):
+        """Questions view should default to 'newest' ordering."""
         self._assert_default_filter_for("qnas:questions")
 
     def test_default_filter_for_tagged_questions(self):
+        """Tagged-questions view should default to 'newest' ordering."""
         self._assert_default_filter_for("qnas:tagged-questions", tag=Tag.objects.create(text="test_tag"))
 
-    def test_defined_tag_for_questions(self):
+    def test_defined_tab_for_questions(self):
+        """Questions view should filter correctly for defined tabs."""
         self._assert_defined_tabs_for("qnas:questions")
 
-    def test_defined_tag_for_tagged_questions(self):
+    def test_defined_tab_for_tagged_questions(self):
+        """Tagged-questions view should filter correctly for defined tabs."""
         self._assert_defined_tabs_for("qnas:tagged-questions", tag=Tag.objects.create(text="test_tag"))
+
 
 class TagViewTests(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.user, = get_user_model().objects.bulk_create((get_user_model()(username=f"author"),))
+        cls.user, = get_user_model().objects.bulk_create(
+            (get_user_model()(username="author"),)
+        )
 
     def test_default_tab_filter(self):
-        """
-        Tab should default to 'popular' if no tab is selected or the selected tab does not exist.
-        """
+        """Tags view should default to 'popular' if no or invalid tab is selected."""
         def _assert_popular_is_default_tab(query_params=None):
             response = _assert_successful_request(self, "qnas:tags", query_params=query_params)
             self.assertEqual(response.context["tab"].lower(), "popular")
 
-        with self.subTest("No tab selected"): _assert_popular_is_default_tab()
-        with self.subTest("Selected undefined tab"): _assert_popular_is_default_tab(query_params={"tab": "unused"})
+        with self.subTest("No tab selected"):
+            _assert_popular_is_default_tab()
+        with self.subTest("Invalid tab selected"):
+            _assert_popular_is_default_tab(query_params={"tab": "unused"})
 
     def test_defined_tabs(self):
+        """
+        'Popular' tab should sort tags by usage count.
+        'New' tab should sort tags by creation date (descending).
+        'Name' tab should sort tags alphabetically.
+        """
         t1, t2, t3 = Tag.objects.bulk_create([Tag(text=str(i)) for i in range(3)])
         q1, q2 = Question.objects.bulk_create([Question(author=self.user) for _ in range(2)])
 
@@ -216,11 +249,11 @@ class TagViewTests(TestCase):
             self.assertEqual(response.context["tab"].lower(), tab.lower())
             return response
 
-        with self.subTest("Tab 'Popular' selected"):
+        with self.subTest("'Popular' tab"):
             self.assertQuerySetEqual(_assert_selected_tab(tab="Popular").context["all_tags"], (t2, t3, t1))
 
-        with self.subTest("Tab 'New' selected"):
+        with self.subTest("'New' tab"):
             self.assertQuerySetEqual(_assert_selected_tab(tab="New").context["all_tags"], (t3, t2, t1))
 
-        with self.subTest("Tab 'Name' selected"):
+        with self.subTest("'Name' tab"):
             self.assertQuerySetEqual(_assert_selected_tab(tab="Name").context["all_tags"], (t1, t2, t3))
